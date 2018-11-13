6b3dbf8b77b990f7e0be06cf057c13d935d3f8cb
Adam Duff <j.adam.duff@gmail.com>
2018-11-13 05:03:47 +0000

    automatically backed up by learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe 'navigate' do
   before do
     @post = Post.create(title: "My Post", description: "My post desc")
-    @post.author = Author.create(name: "Stephen King", hometown: 'Maine')
   end
 
   it 'shows the title on the show page in a h1 tag' do
